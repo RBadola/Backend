0e7f1b68bbda585c8fe2c60198a9068cb8cf8458
Roshan Kumar Badola <67925835+RBadola@users.noreply.github.com>
2023-09-27 16:02:26 +0530

    Update blog.js

--- controllers/blog.js
+++ controllers/blog.js
@@ -18,7 +18,7 @@ export const singleBlog = async (req, res) => {
 //  get All blog 
 export const allBlog = async (req, res) => {
   const blogs = await Blog.find().populate("author","name").sort({ createdAt: -1 })
-  res.status(200).send(blogs)
+  res.status(200).send(req)
 }
 
 // get comments
